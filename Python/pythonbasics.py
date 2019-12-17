23779db8788792e7692051c87a93e1724115b94c
GuyFawkes <calebkibut@gmail.com>
2019-12-17 20:40:05 +0300

    First Python Commit

---
+++ Python/pythonbasics.py
@@ -0,0 +1,24 @@
+print ( "Hello World" )
+my_name="Munoru"
+print(my_name)
+myName="Adam and Eve"
+print(myName)
+myName="Save"
+print(myName)
+x=0
+y=4
+z=x+y
+print(z)
+
+bootcamp="python"
+first=bootcamp[1:5]
+
+sentence ="I, enjoy programming. I am learning python"
+first=sentence[2]
+
+print(first)
+
+first=sentence[1]
+
+print(len(sentence))
+for(i=0;i<=42;i++){ print(sentence[i])}
